77377f83ceae26b7c9d32b099a12ac3b38e7cd47
nickheal <nick3_4@hotmail.com>
2018-04-03 21:03:57 +0100

    Hidden kets

--- site/src/store.js
+++ site/src/store.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import keys from '@/keys';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        googleMapsApiKey: 'xxx'
+        googleMapsApiKey: keys.googleMapsApiKey
     },
     mutations: {
 
